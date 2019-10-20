f743f5eec3b49222ca78730805fb051b706b3c62
AntonSilebin <43464727+AntonSilebin@users.noreply.github.com>
2019-10-20 16:17:43 +0300

    added showing id

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,34 +1,80 @@
 package com.example.lab1;
 
+import android.Manifest;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(getVersion());
+        getTelId();
+        getVersion();
     }
 
-    public String getVersion(){
+    private void getVersion() {
         PackageInfo packageInfo;
-        StringBuilder version = new StringBuilder("ver ");
+        StringBuilder versionStringBuilder = new StringBuilder();
+        versionStringBuilder.append("Version ");
 
-        try{
+        try {
             packageInfo = getApplicationContext()
                     .getPackageManager()
                     .getPackageInfo(getApplicationContext().getPackageName(), 0);
-            version.append(packageInfo.versionName);
+            versionStringBuilder.append(packageInfo.versionName);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        return version.toString();
+        TextView textView = findViewById(R.id.versionView);
+        textView.setText(versionStringBuilder.toString());
+    }
+
+    private void getTelId() {
+        telephonyManager = (TelephonyManager) getSystemService(MainActivity.TELEPHONY_SERVICE);
+        if (checkSelfPermission((Manifest.permission.READ_PHONE_STATE)) == PackageManager.PERMISSION_GRANTED) {
+
+            StringBuilder idStringBuilder = new StringBuilder("ID ");
+            idStringBuilder.append(telephonyManager.getDeviceId());
+
+            TextView textView = findViewById(R.id.idView);
+            textView.setText(idStringBuilder.toString());
+
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
+                Toast.makeText(this, "Permission is needed.", Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && checkSelfPermission((Manifest.permission.READ_PHONE_STATE)) == PackageManager.PERMISSION_GRANTED){
+
+                StringBuilder idStringBuilder = new StringBuilder("\nID ");
+                idStringBuilder.append(telephonyManager.getDeviceId());
+
+                TextView textView = findViewById(R.id.idView);
+                textView.setText(idStringBuilder.toString());
+
+            } else {
+                Toast.makeText(this, "You should grant the permission to get id", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
