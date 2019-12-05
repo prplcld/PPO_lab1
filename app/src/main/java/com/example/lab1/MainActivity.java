3109fbe8a7b7ee6a9b09fb7a4cf86fc331074ae2
AntonSilebin <43464727+AntonSilebin@users.noreply.github.com>
2019-12-05 21:25:51 +0300

    added alert showing

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.lab1;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void getTelId() {
         telephonyManager = (TelephonyManager) getSystemService(MainActivity.TELEPHONY_SERVICE);
+
         if (checkSelfPermission((Manifest.permission.READ_PHONE_STATE)) == PackageManager.PERMISSION_GRANTED) {
 
             StringBuilder idStringBuilder = new StringBuilder("ID ");
@@ -52,29 +55,39 @@ public class MainActivity extends AppCompatActivity {
             textView.setText(idStringBuilder.toString());
 
         } else {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
-                Toast.makeText(this, "Permission is needed.", Toast.LENGTH_LONG).show();
-            }
-            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 1){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && checkSelfPermission((Manifest.permission.READ_PHONE_STATE)) == PackageManager.PERMISSION_GRANTED){
+        switch(requestCode) {
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    StringBuilder idStringBuilder = new StringBuilder("\nID ");
+                    idStringBuilder.append(telephonyManager.getDeviceId());
 
-                StringBuilder idStringBuilder = new StringBuilder("\nID ");
-                idStringBuilder.append(telephonyManager.getDeviceId());
-
-                TextView textView = findViewById(R.id.idView);
-                textView.setText(idStringBuilder.toString());
-
-            } else {
-                Toast.makeText(this, "You should grant the permission to get id", Toast.LENGTH_LONG).show();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                    TextView textView = findViewById(R.id.idView);
+                    textView.setText(idStringBuilder.toString());
+                } else {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("About Permission")
+                            .setMessage("This permission is required to show device id. Would you like to grant permission again?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    alert.show();
+                }
         }
     }
 }
